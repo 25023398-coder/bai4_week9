df51c1d33b971d4125462ac3f0cfc19af71e1a62
Phuong <25023398@vnu.edu.vn>
2026-04-27 23:42:40 +0700

    sua loi lan 3

--- src/test/java/com/banksystem/FileLoggerTest.java
+++ src/test/java/com/banksystem/FileLoggerTest.java
@@ -12,10 +12,4 @@ public class FileLoggerTest {
         String result = logger.getLogPath("logs", "app.log");
         assertTrue(result.contains(File.separator));
     }
-    @Test
-    void testHardcodedPathFailure() {
-        String badPath = "logs\\app.log"; 
-        assertTrue(badPath.contains(File.separator), 
-            "loi: duong dan cung Windows se that bai tren moi truong Matrix khac");
-    }
 }
